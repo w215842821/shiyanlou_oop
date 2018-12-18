fa075d50da501644a1dacbd86d8f382a2f336461
w215842821 <215842821@qq.com>
2018-12-18 09:49:27 +0800

    lianxi

--- classtest.py
+++ classtest.py
@@ -7,18 +7,8 @@ class UserData(object):
 	def __repr__(self):
 		return "ID:{} Name:{}".format(self.id, self.name)
 
-class NewUser(UserData):
-	def get_name(self):
-		return self.name
-
-	def set_name(self,value):
-		self.name = value
-
-		
-
 if __name__ == '__main__':
-	user1 = NewUser(101, 'Jack')
-	user1.set_name('Jackie')
+	user1 = UserData(101, 'Jack')
 	user2 = UserData(102,'Louplus')
 	print(user1)
 	print(user2)
